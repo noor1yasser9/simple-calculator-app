7366ca602656a36bdfc6a9a1a3ccb6c51545cbfe
Nour Yasser <41232970+noor1yasser9@users.noreply.github.com>
2026-04-14 19:07:43 +0200

    [AgentFlow] تنفيذ الحساب وعرض النتيجة - script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function resetState() {
-  state.lastOperand  = null;
-  state.lastOperator = null;
-  // ...
+function animateEquals() {
+  equalsBtns.forEach((btn) => {
+    btn.classList.remove("is-calculating");
+    void btn.offsetWidth; // إعادة تشغيل الـ animation
+    btn.classList.add("is-calculating");
+  });
 }
