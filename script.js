70db259b2353145446fe9bc09b670b079b224dad
Nour Yasser <41232970+noor1yasser9@users.noreply.github.com>
2026-04-14 18:52:55 +0200

    [AgentFlow] إنشاء ملف index.html وهيكل الكالكيوليتر - script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const state = {
+  current:     "0",    // الرقم الحالي
+  previous:    "",     // الرقم السابق
+  operator:    null,   // العملية المحددة
+  shouldReset: false,  // إعادة ضبط عند الإدخال التالي
+};
